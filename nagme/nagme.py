1055dada96f57add9d5411b3a9aa103a4c44f387
Artiom Basenko <demi.log@gmail.com>
2014-03-02 13:59:32 +0400

    Playing with enaml.

--- nagme/nagme.py
+++ nagme/nagme.py
@@ -4,17 +4,18 @@
 import enaml
 from enaml.qt.qt_application import QtApplication
 
-from models.model import Person
+#from models.model import Person
 
 if __name__ == '__main__':
     with enaml.imports():
-        from views.view import PersonView
+        from views.view import Main
 
-    john = Person(first_name='John', last_name='Doe')
+    #john = Person(first_name='John', last_name='Doe')
 
     app = QtApplication()
 
-    view = PersonView(person=john)
+    #view = PersonView(person=john)
+    view = Main()
     view.show()
 
     app.start()
